2cddef3e07a68963b36032ff1195331294cdc01b
Prajwal-S-6 <prajwals12021999@gmail.com>
2025-10-29 09:35:07 +0530

    custom-radio-button

--- projects/demo-app/src/app/app.ts
+++ projects/demo-app/src/app/app.ts
@@ -1,11 +1,11 @@
 import {Component, inject, OnInit, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {KpInput} from 'custom-ui';
+import {KpInput, KpRadioButton} from 'custom-ui';
 import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, KpInput, ReactiveFormsModule],
+  imports: [RouterOutlet, KpInput, ReactiveFormsModule, KpRadioButton],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
@@ -13,10 +13,16 @@ export class App implements OnInit{
   protected readonly title = signal('demo-app');
   fb = inject(FormBuilder);
   myForm!: FormGroup;
+  genderOptions = [
+    { label: 'Male', value: 'M', checked: true },
+    { label: 'Female', value: 'F', checked: false },
+    { label: 'Other', value: 'O', checked: false }
+  ];
 
   ngOnInit(): void {
     this.myForm = new FormGroup({
-      myName: new FormControl('Prajwal')
+      myName: new FormControl('Prajwal'),
+      myGender: new FormControl('M')
     })
   }
 
